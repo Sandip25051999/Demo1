6702440da049d9b6b0722064ce7471ac11c5ac5e
Sandip Phanse <phansesandip92@gmail.com>
2021-12-01 20:26:18 +0530

    Second Commit

--- Demo1.java
+++ Demo1.java
@@ -4,7 +4,8 @@ public class Demo1 {
 
 public static void main(String[] args) {
 System.out.println("Hello World!!");
-
+System.out.println("Hello Sandip");
+System.out.println("Hello Jenkins");
 
 
 
